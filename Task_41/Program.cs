394647a942e8daf84c1da858a30876eebde02556
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-07 13:38:53 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Сколько чисел вы хотите ввести = ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+double[] array = new double[m];
+for(int i = 0; i < m; i++){
+    Console.WriteLine("Введите число " + (i + 1));
+    array[i] = Convert.ToDouble(Console.ReadLine());
+    if(array[i] > 0){
+        count++;
+    }
+}
+Console.WriteLine("Чисел больше нуля = " + count);
